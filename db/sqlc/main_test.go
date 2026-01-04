951f849b01daf1f893d13dbc57d312dd96d30e61
aabiskar <manojkhatri2069@gmail.com>
2026-01-04 12:19:31 +0000

    chore: use viper for environment variable configuration

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,22 +6,28 @@ import (
 	"os"
 	"testing"
 
-	_ "github.com/lib/pq"
-)
+	"github.com/aabiskar/simplebank/util"
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	log.Printf("Loaded config for test: %+v", config)
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	//
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
+	}
+	if err := testDB.Ping(); err != nil {
+		log.Fatal("cannot ping db:", err)
 	}
 
 	log.Println("connected to db")
--- main.go
+++ main.go
@@ -6,17 +6,24 @@ import (
 
 	"github.com/aabiskar/simplebank/api"
 	db "github.com/aabiskar/simplebank/db/sqlc"
+	"github.com/aabiskar/simplebank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
+// const (
+// 	dbDriver      = "postgres"
+// 	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+// 	serverAddress = "0.0.0.0:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	log.Printf("Loaded config: %+v", config)
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +31,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
